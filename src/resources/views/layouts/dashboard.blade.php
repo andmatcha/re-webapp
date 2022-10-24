ed071b21351de183ab62084f310ac40d211d5240
andmatcha <jj.mcr185@gmail.com>
2022-10-25 02:04:40 +0900

    feat: :sparkles: jsの読み込み

--- src/resources/views/layouts/dashboard.blade.php
+++ src/resources/views/layouts/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title')</title>
 </head>
 
