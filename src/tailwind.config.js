87994e72b22e5b4173ccf57a15b6ca893056abe9
andmatcha <jj.mcr185@gmail.com>
2022-10-25 01:19:46 +0900

    feat: :sparkles: 独自カラーパレットの設定

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-blue': '#0f71ba',
+        'main-light-blue': '#96b9d0',
+        'main-bg-grey': '#f5f5f8'
+      }
+    },
   },
   plugins: [],
 }
