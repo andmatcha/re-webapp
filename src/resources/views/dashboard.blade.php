188b4f07559cbc80d2f4083d2201b90d79451022
andmatcha <jj.mcr185@gmail.com>
2022-10-25 00:11:59 +0900

    feat: :sparkles: bladeの大まかな位置取り

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -6,13 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Document</title>
+    <title>ダッシュボード | POSSE WEB APP</title>
 </head>
 
 <body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <header>
+        {{-- ロゴ week表示 ログアウトボタン 記録・投稿ボタン --}}
+    </header>
+    <main>
+        <section>
+            {{-- 学習時間表示&日別学習時間棒グラフ --}}
+            <div>
+                {{-- 学習時間表示 --}}
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                {{-- 日別学習時間棒グラフ --}}
+            </div>
+        </section>
+        <section>
+            {{-- 学習言語 --}}
+        </section>
+        <section>
+            {{-- 学習コンテンツ --}}
+        </section>
+    </main>
+    <footer>
+        {{-- 月表示 月移動ボタン --}}
+    </footer>
 </body>
 
 </html>
