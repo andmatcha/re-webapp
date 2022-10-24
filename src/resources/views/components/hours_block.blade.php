001115422d0a571fd438ee9c96cc5cb294c631d9
andmatcha <jj.mcr185@gmail.com>
2022-10-25 01:19:24 +0900

    feat: :sparkles: 時間表示ブロックのコンポーネント

---
+++ src/resources/views/components/hours_block.blade.php
@@ -0,0 +1,5 @@
+<div class="flex flex-col items-center justify-center bg-white rounded-xl w-fit py-4 grow">
+    <h3 class="text-main-blue font-bold text-xl tracking-widest">{{ $title }}</h3>
+    <p class="text-5xl font-bold">{{ $amount }}</p>
+    <p class="text-main-light-blue text-xl">hours</p>
+</div>
