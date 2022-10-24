214e340e35292db7fd38e6227081d0a8e67fd0ba
andmatcha <jj.mcr185@gmail.com>
2022-10-24 21:20:21 +0900

    chore: :wrench: update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js'
+  ],
   content: [],
   theme: {
     extend: {},
