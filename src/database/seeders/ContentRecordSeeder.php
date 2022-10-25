100d6f949028b510681e029b8c24d3db263e6bb3
andmatcha <jj.mcr185@gmail.com>
2022-10-26 01:30:00 +0900

    fix: :seedling: seeder更新

--- src/database/seeders/ContentRecordSeeder.php
+++ src/database/seeders/ContentRecordSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -15,6 +16,16 @@ class ContentRecordSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('content_record')->insert([]);
+        DB::table('content_record')->insert([
+            ['record_id' => 1, 'content_id' => 1, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 1, 'content_id' => 3, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 2, 'content_id' => 1, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 2, 'content_id' => 2, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 2, 'content_id' => 3, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 3, 'content_id' => 2, 'amount' => 2, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 3, 'content_id' => 3, 'amount' => 2, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'content_id' => 1, 'amount' => 2.5, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'content_id' => 2, 'amount' => 2.5, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
     }
 }
--- src/database/seeders/LanguageRecordSeeder.php
+++ src/database/seeders/LanguageRecordSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LanguageRecordSeeder extends Seeder
 {
@@ -14,6 +16,17 @@ class LanguageRecordSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('language_record')->insert([]);
+        DB::table('language_record')->insert([
+            ['record_id' => 1, 'language_id' => 2, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 1, 'language_id' => 5, 'amount' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 2, 'language_id' => 4, 'amount' => 1.5, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 2, 'language_id' => 7, 'amount' => 1.5, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 3, 'language_id' => 1, 'amount' => 2, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 3, 'language_id' => 5, 'amount' => 2, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'language_id' => 4, 'amount' => 1.25, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'language_id' => 5, 'amount' => 1.25, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'language_id' => 6, 'amount' => 1.25, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['record_id' => 4, 'language_id' => 8, 'amount' => 1.25, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
     }
 }
